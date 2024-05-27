18194b40fd56a214859bbaad949a5b0a81385d55
Aleksandra Jaroszek <107071325+parabola01@users.noreply.github.com>
2024-05-27 15:50:41 +0200

    update project

--- config/database.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize');
-let sequelize;
-sequelize = new Sequelize('project_db', 'root', 'root', {
-    host: 'localhost',
-    dialect: 'mysql'
-});
-
-module.exports = sequelize;
